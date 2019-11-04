10a1a453fd85758c924b7c6e3d1af13a31470e22
Elena Kulakova <kulakovaelena15@gmail.com>
2019-11-04 21:13:45 +0100

    Initial Commit

---
+++ Ask Me Anyting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ask Me Anyting
+//
+//  Created by Darko Kulakov on 2019-11-04.
+//  Copyright © 2019 Elena Kulakova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
